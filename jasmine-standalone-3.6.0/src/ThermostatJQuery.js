d6bfd941b11bd35f4d4c3621f16bce1170da7137
Hamish Arro <Hamisharro@icloud.com>
2021-02-12 16:16:14 +0000

    setting up api

--- jasmine-standalone-3.6.0/src/ThermostatJQuery.js
+++ jasmine-standalone-3.6.0/src/ThermostatJQuery.js
@@ -1,32 +1,45 @@
 $(document).ready(() => {
   let thermostat = new Thermostat();
-  updateEnergyUsage = () => $('.energyUsage').text(thermostat.energyUsage());
-  updateTemperature = () => $('#temperature').text(thermostat.currentTemperature);
+  updateTemperature = () => {
+    $('#temperature').text(thermostat.currentTemperature);
+    $('#temperature').attr('class', thermostat.energyUsage());
+    // $('.energyUsage').text(thermostat.energyUsage())
+  };
   updateTemperature();
 
   $('#increase_temp').click(() => {
     thermostat.increaseTemperature();
     updateTemperature();
-    updateEnergyUsage();
   })
 
   $('#decrease_temp').click(() => {
     thermostat.decreaseTemperature();
     updateTemperature();
-    updateEnergyUsage();
   })
 
   $('#powerSaveButton').click(() => {
     thermostat.powerSavingSwitch();
     thermostat.isPowerSaving() ? $('#powersave').text('on') : $('#powersave').text('off');
     updateTemperature();
-    updateEnergyUsage();
   })
 
   $('.reset').click(() => {
     thermostat.reset();
     updateTemperature();
-    updateEnergyUsage();
   })
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 })
